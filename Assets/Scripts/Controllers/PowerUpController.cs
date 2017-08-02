5f026640d07ee3c0c0a3922b24019b81f930104f
Robert French <rfrench@robertfrench.ca>
2017-08-02 18:09:03 -0400

    don;t even know what I did

--- Assets/Scripts/Controllers/PowerUpController.cs
+++ Assets/Scripts/Controllers/PowerUpController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 public enum SuperPower { BREAK, CATCH, DISRUPT, ENLARGE, LASER, LIFE, SLOW }; 
